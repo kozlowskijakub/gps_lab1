630f0e47ab85648e5b8aafca116e2e879f0254af
jakub <jakub@jakub-Lenovo-G580.(none)>
2013-10-10 15:09:03 +0200

    corrected function counting distance between objects

--- gen/com/example/lab_gps2/R.java
+++ gen/com/example/lab_gps2/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
         public static final int btn_addPoint=0x7f080004;
         public static final int tv_direction=0x7f080003;
         public static final int tv_lattitude=0x7f080001;
         public static final int tv_longitude=0x7f080000;
         public static final int tv_poi_direction=0x7f080006;
         public static final int tv_poi_distance=0x7f080007;
+        public static final int tv_poi_distance2=0x7f080008;
         public static final int tv_points_number=0x7f080005;
         public static final int tv_speed=0x7f080002;
     }
--- src/com/example/lab_gps2/MainActivity.java
+++ src/com/example/lab_gps2/MainActivity.java
@@ -9,11 +9,11 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements LocationListener {
 
@@ -21,10 +21,11 @@ public class MainActivity extends Activity implements LocationListener {
 	public TextView longitude;
 	public TextView direction;
 	public TextView speed;
-	public TextView poiDistance;
+//	public TextView poiDistance;
+	public TextView poiDistance2;
 	public TextView poiDirection;
 	public TextView poiNumber;
-	public static final Double Radius = 6371d;
+	public static final Double Radius = 6371000d;
 
 	Poi currentPoi;
 	List<Poi> listPoi;
@@ -44,9 +45,10 @@ public class MainActivity extends Activity implements LocationListener {
 		this.direction = (TextView) findViewById(R.id.tv_direction);
 		this.speed = (TextView) findViewById(R.id.tv_speed);
 		this.poiDirection = (TextView) findViewById(R.id.tv_poi_direction);
-		this.poiDistance = (TextView) findViewById(R.id.tv_poi_distance);
+//		this.poiDistance = (TextView) findViewById(R.id.tv_poi_distance);
+		this.poiDistance2 = (TextView) findViewById(R.id.tv_poi_distance2);
 		this.poiNumber = (TextView) findViewById(R.id.tv_points_number);
-		
+
 		currentPoi = new Poi();
 		listPoi = new ArrayList<Poi>();
 
@@ -66,31 +68,50 @@ public class MainActivity extends Activity implements LocationListener {
 	public void addPoi(View v) {
 		Poi poi = this.currentPoi.clone();
 		this.listPoi.add(poi);
-		this.poiNumber.setText(String.valueOf(this.clicks+1));
+		this.poiNumber.setText(String.valueOf(this.clicks + 1));
 
 		if (clicks > 0) {
-			try{
-				String distance = String.valueOf(countDistance(
-						this.currentPoi, listPoi.get(listPoi.size() - 2)));
-			this.poiDistance.setText(distance);
-			this.poiDirection.setText(String.valueOf(countBearing(
-					this.currentPoi, listPoi.get(listPoi.size() - 2))));
-			}catch(Exception e){
-				Log.e("parseException", e.toString());
+			try {
+//				this.poiDistance.setText(String.valueOf(countDistance(this.currentPoi,
+//						listPoi.get(listPoi.size() - 2))));
+				this.poiDistance2.setText(String.valueOf(countDistance2(this.currentPoi,
+						listPoi.get(listPoi.size() - 2))));
+				
+				this.poiDirection.setText(String.valueOf(countBearing(
+						this.currentPoi, listPoi.get(listPoi.size() - 2))));
+			} catch (Exception e) {
+				// Log.e("parseException", e.toString());
+				Toast.makeText(MainActivity.this, e.toString(),
+						Toast.LENGTH_LONG).show();
 			}
 		}
 		clicks++;
 	}
 
-	public Double countDistance(Poi poi1, Poi poi2) {
-		Double distance;
-		Double x, y;
-		x = (poi1.longitude - poi2.longitude)
-				* Math.cos((poi1.longitude + poi2.longitude) / 2);
-		y = (poi1.lattitude - poi2.lattitude);
-		distance = Radius * Math.sqrt((x * x) + (y * y));
-		return distance*1000;
+	public Double countDistance2(Poi poi1, Poi poi2) {
+
+		Double dLat = Math.toRadians(poi2.lattitude - poi1.lattitude);
+		Double dLon = Math.toRadians(poi2.longitude - poi1.longitude);
+		Double lat1 = Math.toRadians(poi1.lattitude);
+		Double lat2 = Math.toRadians(poi2.lattitude);
+
+		Double x = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2)
+				* Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat1);
+		Double y = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x));
+		Double distance = Radius*y;
+
+		return distance;
 	}
+
+//	public Double countDistance(Poi poi1, Poi poi2) {
+//		Double distance;
+//		Double x, y;
+//		x = (poi1.longitude - poi2.longitude)
+//				* Math.cos((poi1.longitude + poi2.longitude) / 2);
+//		y = (poi1.lattitude - poi2.lattitude);
+//		distance = Radius * Math.sqrt((x * x) + (y * y));
+//		return distance;
+//	}
 
 	public double countBearing(Poi poi1, Poi poi2) {
 		Double lat1 = poi1.lattitude;
